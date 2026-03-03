2b0107670cbd8d12864724f82ea0be678ad44445
labpnet33 <labpnet33@gmail.com>
2026-03-03 23:33:47 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -383,3 +383,16 @@ if __name__ == "__main__":
     target = sys.argv[1]
     results = scan_and_analyze(target)
     print(json.dumps(results, indent=2))
+
+    if "--discover" in sys.argv:
+    idx=sys.argv.index("--discover")
+    print(json.dumps(network_discovery(sys.argv[idx+1]),indent=2))
+else:
+    mods=["ports","ssl","dns","headers"]
+    if "--modules" in sys.argv:
+        idx=sys.argv.index("--modules")
+        mods=sys.argv[idx+1].split(",")
+        target=sys.argv[-1]
+    else:
+        target=sys.argv[1]
+    print(json.dumps(full_scan(target,mods),indent=2))
